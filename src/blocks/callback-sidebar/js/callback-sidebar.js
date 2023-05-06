7b84f5e2080c38ff1d64bf1e6c5e83a490f4b884
Mаксимов Илья <ilya.maximov11@gmail.com>
2023-05-06 20:51:49 +0300

    fix html errors

--- src/blocks/callback-sidebar/js/callback-sidebar.js
+++ src/blocks/callback-sidebar/js/callback-sidebar.js
@@ -1,6 +1,6 @@
 import {toggleCallbackSidebar, disableAllSidebars} from '../../../default/js/sidebars-togglers.js'
 
-let callbackButtons = document.querySelectorAll('#callback-open');
+let callbackButtons = document.querySelectorAll('.callback-open');
 let callbackButtonClose = document.getElementById('callback-close');
 
 callbackButtons.forEach(button => {
